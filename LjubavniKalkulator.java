6001464e214229986dd2ed4f5fcf2df5ca6aa3f9
Goran Franjic <goran@proces.hr>
2023-08-22 15:22:26 +0200

    Update LjubavniKalkulator.java

--- LjubavniKalkulator.java
+++ LjubavniKalkulator.java
@@ -132,3 +132,108 @@ public class LjubavniKalkulator {
 	}
 	
 }
+
+
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LjubavniKalkulator {
+
+    public static void main(String[] args) {
+        Scanner unos = new Scanner(System.in);
+
+        System.out.println("Unesite prvo ime:");
+        String ime1 = unos.nextLine();
+
+        System.out.println("Unesite drugo ime:");
+        String ime2 = unos.nextLine();
+
+        String s = ime1.toLowerCase() + ime2.toLowerCase();
+
+        int[] niz = new int[ime1.length() + ime2.length()];
+
+        int b;
+        char z;
+        for (int i = 0; i < s.length(); i++) {
+            z = s.charAt(i);
+            b = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == z) {
+                    b++;
+                }
+            }
+            niz[i] = b;
+        }
+
+        // Transformacija niza nizDva (prva petlja)
+        int[] nizDva = Arrays.copyOf(niz, niz.length);
+        int duljina = nizDva.length;
+        StringBuilder nizTri = new StringBuilder();
+
+        for (int i = 0; i < duljina / 2; i++) {
+            nizTri.append(nizDva[i] + nizDva[duljina - 1 - i]);
+        }
+
+        if (duljina % 2 != 0) {
+            nizTri.append(nizDva[duljina / 2]);
+        }
+
+        System.out.println(nizTri);
+
+        // Transformacija niza novoNiz (druga petlja)
+        int[] novoNiz = new int[nizTri.length()];
+        for (int i = 0; i < nizTri.length(); i++) {
+            novoNiz[i] = Character.getNumericValue(nizTri.charAt(i));
+        }
+
+        duljina = novoNiz.length;
+        nizTri = new StringBuilder();
+
+        for (int i = 0; i < duljina / 2; i++) {
+            nizTri.append(novoNiz[i] + novoNiz[duljina - 1 - i]);
+        }
+
+        if (duljina % 2 != 0) {
+            nizTri.append(novoNiz[duljina / 2]);
+        }
+
+        System.out.println("Rezultat druge petlje je: " + nizTri);
+
+        // Transformacija niza zadnjiNiz (treća petlja)
+        int[] zadnjiNiz = new int[nizTri.length()];
+        for (int i = 0; i < nizTri.length(); i++) {
+            zadnjiNiz[i] = Character.getNumericValue(nizTri.charAt(i));
+        }
+
+        duljina = zadnjiNiz.length;
+        nizTri = new StringBuilder();
+
+        for (int i = 0; i < duljina / 2; i++) {
+            nizTri.append(zadnjiNiz[i] + zadnjiNiz[duljina - 1 - i]);
+        }
+
+        if (duljina % 2 != 0) {
+            nizTri.append(zadnjiNiz[duljina / 2]);
+        }
+
+        System.out.println("Rezultat zadnje petlje je: " + nizTri);
+
+        // Pozivanje funkcije PostupakBezIspisa
+        int[] rezultat = PostupakBezIspisa(zadnjiNiz);
+        System.out.println("Rezultat funkcije PostupakBezIspisa: " + Arrays.toString(rezultat));
+    }
+
+    public static int[] PostupakBezIspisa(int[] brojevi) {
+        // Implementacija tvoje funkcije PostupakBezIspisa
+        // ...
+        return brojevi2;
+    }
+
+    public static void main(String[] args) {
+        // Pozivanje glavnog main-a
+        main(args);
+    }
+}
+
